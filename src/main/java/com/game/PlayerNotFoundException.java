7fa8733de1bc9d77c1dcc655a34c4fbdfb378909
Yulia Platonova <julia.ikkunaprincessa@gmail.com>
2021-05-21 11:01:09 -0700

    exceptions classes added

---
+++ src/main/java/com/game/PlayerNotFoundException.java
@@ -0,0 +1,22 @@
+package com.game;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class PlayerNotFoundException extends RuntimeException{
+    public PlayerNotFoundException() {
+    }
+
+    public PlayerNotFoundException(String message) {
+        super(message);
+    }
+
+    public PlayerNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PlayerNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
