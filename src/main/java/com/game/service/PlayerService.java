4109c67dca9af153cade46382971fa2b0a542376
Yulia Platonova <julia.ikkunaprincessa@gmail.com>
2021-05-21 10:42:55 -0700

    simple PlayerService added

---
+++ src/main/java/com/game/service/PlayerService.java
@@ -0,0 +1,24 @@
+package com.game.service;
+
+
+import com.game.entity.Player;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.List;
+
+public interface PlayerService {
+    List<Player> getAllPlayers(Specification<Player> specification);
+
+    Page<Player> getAllPlayers(Specification<Player> specification, Pageable sortedByName);
+
+    Player createPlayer(Player requestPlayer);
+
+    Player getPlayerById(Long id);
+
+    Player updatePlayerById(Long id, Player player);
+
+    void deletePlayerById(Long id);
+
+}
